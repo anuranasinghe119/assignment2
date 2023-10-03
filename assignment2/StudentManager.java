0eeec98d7231abc1abba6cc7b707392204ea3596
Anupa Ranasinghe <anuranasinghe119@gmail.com>
2023-10-03 11:37:19 +1100

    implement inner class variables

---
+++ assignment2/StudentManager.java
@@ -0,0 +1,15 @@
+
+/**
+ * This is a program to develop a simple program to compute statistics of students' marks in an assignment.
+ *
+ * @author (anupa)
+ * @version (version 1.0 28/09/2023)
+ */
+public class StudentManager
+{
+    // Inner class to represent individual students
+    String name;
+    String studentID;
+    int[] marks = new int[3];
+    int totalMark;
+}
